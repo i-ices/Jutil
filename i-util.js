6970f74c0f4e4ea5ffec04c2bdf0967d1f969488
i-ices <69052395+i-ices@users.noreply.github.com>
2022-06-26 18:05:27 +0800

    Add files via upload

---
+++ i-util.js
@@ -0,0 +1,64 @@
+const $ = (function () {
+    const q = {};
+
+    Object.assign(q, {
+        attr,
+        css,
+        dom,
+        each,
+        on,
+    })
+
+    return (function (selector) {
+        Object.assign(q, {
+            selector,
+            nodes: selector instanceof Element ? [selector] : document.querySelectorAll(selector),
+        })
+        return q;
+    })
+
+    function attr(prop, val) {
+        if (Boolean(val)) {
+            this.each(function (node) {
+                Element.prototype.setAttribute.apply(node, [prop, val]);
+            });
+            return this;
+        } else {
+            return Array.prototype.map.call(this.nodes, function (node) {
+                return node.getAttribute(prop);
+            });
+        }
+    }
+
+    function css(prop, val) {
+        if (Boolean(val)) {    //setter
+            this.each(function (node) {
+                CSSStyleDeclaration.prototype.setProperty.apply(node.style, [prop, val]);
+            });
+            return this
+        } else {                   //getter
+            return this.nodes[0].style[prop];
+        }
+    }
+
+    function dom() {
+        let arr = [...this.nodes];
+        return arr.length === 1 ? arr[0] : arr;
+    }
+
+    function each(action) {
+        this.nodes.forEach(function (item, i) {
+            action(item, i)
+        })
+        return this;
+    }
+
+    function on(type, callback) {
+        this.each(function (node) {
+            node['on' + type] = callback
+        })
+        return this;
+    }
+})();
+
+console.log($('div').dom())
